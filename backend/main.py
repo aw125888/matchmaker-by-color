73fdfbf9fecbeb317868467c22b88f95b436eba3
Amber Wu <aw125@wellesley.edu>
2026-08-24 04:34:00 -0700

    updated CORS to allow render

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,9 @@ class MatchRequest(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://matchmaker-by-color.vercel.app",],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
